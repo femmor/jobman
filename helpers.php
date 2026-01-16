18b57fc92ba6c5b6b011dae542ffc0be99eca0b0
Emmanuel Egomson <fegomson@gmail.com>
2026-01-16 17:24:11 -0600

    feat: implement custom view loading functions and restructure home view with partials

--- helpers.php
+++ helpers.php
@@ -5,6 +5,49 @@
  * @param string $path
  * @return string
  */
-function basePath($path = '') {
+function basePath($path = '')
+{
     return __DIR__ . '/' . $path;
-}   
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name  
+ * @return void
+ */
+
+function loadView($name)
+{
+
+    $viewPath =  basePath("views/$name.view.php");
+
+    // Check if the view file exists
+    if (!file_exists($viewPath)) {
+        echo "View file not found: $viewPath";
+        throw new Exception("View file not found: $viewPath");
+    }
+
+    require $viewPath;
+}
+
+
+/**
+ * Load partials
+ * Load a partial view
+ * 
+ * @param string $name   The name of the partial view
+ * @return void
+ */
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/$name.php");
+
+    // Check if the partial file exists
+    if (!file_exists($partialPath)) {
+        echo "Partial file not found: $partialPath";
+        throw new Exception("Partial file not found: $partialPath");
+    }
+
+    require $partialPath;
+}
