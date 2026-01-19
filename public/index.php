1a41dcb645a812017249eb5a3389ed2fc4d89442
Emmanuel Egomson <fegomson@gmail.com>
2026-01-19 17:48:19 -0600

    feat: refactor routing logic by separating routes and router into distinct files

--- public/index.php
+++ public/index.php
@@ -1,20 +1,7 @@
 <?php
 require '../helpers.php';
 
-// Routes assoc array
-$routes = [
-    '/' => 'controllers/home.php',
-    '/login' => 'controllers/login.php',
-    '/register' => 'controllers/register.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
+$routes = require basePath('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-if (array_key_exists($uri, $routes)) {
-    require basePath($routes[$uri]);
-} else {
-    require basePath($routes['404']);
-}
+require basePath('router.php');
---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    '/' => 'controllers/home.php',
+    '/login' => 'controllers/login.php',
+    '/register' => 'controllers/register.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
