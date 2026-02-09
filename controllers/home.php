3cc94240fc447e1efe37dcdb7e597a46211eb50e
Emmanuel Egomson <fegomson@gmail.com>
2026-02-09 13:52:48 -0600

    feat: enhance job listings display by removing htmlspecialchars for dynamic content and updating detail links

--- controllers/home.php
+++ controllers/home.php
@@ -7,6 +7,8 @@
 $config = require basePath('config/db.php');
 $db = new Database($config);
 
+// Fetches job listings
 $listings = $db->query("SELECT * FROM listings LIMIT 6")->fetchAll();
 
+// Load the home view with listings data
 loadView('home', ['listings' => $listings]);
